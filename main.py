019bcfaca1b4cdc3bfdc1049af702ba6e01f5823
Son <tranthanhsonwork1999@gmail.com>
2019-08-09 16:27:19 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_boston
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+X, y = load_boston(True)
+params = {'n_estimators': 100, 'max_depth': 4, 'random_state': 0}
+
+regressor = ExtraTreesRegressor(**params)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+regressor.fit(X_train, y_train)
+
+print("Mean absolute error :", round(mean_absolute_error(y_test, regressor.predict(X_test))))
+
