5c822e11381413a38bf706cd8474b09ab9f5f5c8
Son <tranthanhsonwork1999@gmail.com>
2019-08-10 12:36:15 +0700

    Optimized paramenters

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import numpy
 from sklearn.datasets import load_boston
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold, GridSearchCV
 
 X, y = load_boston(True)
-params = {'n_estimators': 100, 'max_depth': 4, 'random_state': 0}
+parameters = {'max_depth': 84, 'n_estimators': 25}
 
-regressor = ExtraTreesRegressor(**params)
+regressor = ExtraTreesRegressor(**parameters)
 
+# kfold = KFold(n_splits=8, random_state=6, shuffle=True)
+
+# grid = GridSearchCV(estimator=regressor, param_grid=parameter_grid, cv=kfold)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 regressor.fit(X_train, y_train)
